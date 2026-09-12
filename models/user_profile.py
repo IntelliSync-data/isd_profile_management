b1ebff62e9200129fcf63ae0e1968bd11f4cbf8d
Huan Nguyen <huan.nguyen@intellisyncdata.com>
2026-09-12 21:57:40 +0700

    fix: explicitly reference print label wizard view to show Print button instead of Save

--- models/user_profile.py
+++ models/user_profile.py
@@ -787,6 +787,7 @@ class UserStep(models.Model):
             'name': _('Print Label'),
             'res_model': 'print.label.wizard',
             'view_mode': 'form',
+            'view_id': self.env.ref('isd_profile_management.view_print_label_wizard_form').id,
             'target': 'new',
             'context': {
                 'default_user_step_id': self.id,
